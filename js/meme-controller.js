706c7842e9a7caf7ab1536203393faf40516b7e7
Iris Rifold <irisrifold@gmail.com>
2020-06-11 10:57:56 +0300

    fixed moving per line and font change

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -4,10 +4,11 @@
 var gElCanvas;
 var gCtx;
 var gImg;
-var gTextUp;
+// var gTextUp;
+// var gTextDown;
 var gMeme;
 var gFontSize = 30;
-var gHeight = gFontSize + 10;
+// var gHeight = gFontSize + 10;
 
 function onOpenModel(id) {
     document.querySelector('.gallery-container').style.display = 'none';
@@ -40,14 +41,28 @@ function drawImgFromlocal(url) {
 
 function onAddText(elInput) {
     clearCanvas();
-    gTextUp = elInput.value;
-    renderMeme(gTextUp);
-    gMeme.lines[0] = gTextUp;
+    if (elInput.name === 'line1') {
+        gMeme.selectedLineIdx = 0;
+        gMeme.lines[0] = {
+            txt: elInput.value,
+            size: 30,
+            height: 40
+        };
+    } else {
+        gMeme.selectedLineIdx = 1;
+        gMeme.lines[1] = {
+            txt: elInput.value,
+            size: 30,
+            height: gElCanvas.height - 10
+        };
+
+    }
+    renderMeme()
 }
 
-function drawText(text, align, x, y) {
+function drawText(text, index, align, x, y) {
     gCtx.lineWidth = '2';
-    gCtx.font = `${gFontSize}px impact`;
+    gCtx.font = `${gMeme.lines[gMeme.selectedLineIdx].size}px impact`;
     gCtx.textAlign = 'center';
     gCtx.strokeStyle = 'black';
     gCtx.fillStyle = 'white';
@@ -55,30 +70,46 @@ function drawText(text, align, x, y) {
     gCtx.fillText(text, x, y);
 }
 
-function onChangeFontSize(isAdd) {
-    (isAdd) ? gFontSize += 10 : gFontSize -= 10;
-    console.log(gFontSize);
-    renderMeme(gTextUp);
+function onChangeFontSize(isAddFontSize) {
+    if (!gMeme.lines) return
+    (isAddFontSize) ? gMeme.lines[gMeme.selectedLineIdx].size += 10 : gMeme.lines[gMeme.selectedLineIdx].size -= 10;
+    if (gMeme.selectedLineIdx === 0) {
+        (isAddFontSize) ? gMeme.lines[gMeme.selectedLineIdx].height = gMeme.lines[gMeme.selectedLineIdx].size += 10 : 
+        gMeme.lines[gMeme.selectedLineIdx].height = gMeme.lines[gMeme.selectedLineIdx].size -= 5
+    } 
+    renderMeme();
 }
 
-function onChangeHeight(isUp) {
-    if (!gTextUp) return;
-    if (isUp) {
-        if (gHeight === 30) return;
-        gHeight -= 10;
+function onChangeHeight(isMovingUp) {
+    if (isMovingUp) {
+        if (gMeme.lines[gMeme.selectedLineIdx].height < gFontSize + 10) return;
+        gMeme.lines[gMeme.selectedLineIdx].height -= 10;
     } else {
-        if (gHeight === 530) return;
-        gHeight += 10;
+        if (gMeme.lines[gMeme.selectedLineIdx].height >= 530) return;
+        gMeme.lines[gMeme.selectedLineIdx].height += 10;
     }
-    renderMeme(gTextUp);
+    renderMeme();
 }
 
 function clearCanvas() {
     gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
 }
 
-function renderMeme(txt) {
+function renderMeme() {
     gCtx.drawImage(gImg, 0, 0, gElCanvas.width, gElCanvas.height);
     var xCenter = gElCanvas.width / 2;
-    drawText(txt, 'center', xCenter, gHeight);
+    if (gMeme.lines[0]) {
+        drawText(gMeme.lines[0].txt, 0, 'center', xCenter, gMeme.lines[0].height);
+        if (gMeme.lines[1]) drawText(gMeme.lines[1].txt, 1, 'center', xCenter, gMeme.lines[1].height);
+    }
+    if (gMeme.lines[1]) {
+        drawText(gMeme.lines[1].txt, 1, 'center', xCenter, gMeme.lines[1].height);
+        if (gMeme.lines[0]) drawText(gMeme.lines[0].txt, 0, 'center', xCenter, gMeme.lines[0].height);
+    }
+}
+
+
+function onSenlectedInput(num) {
+    gMeme.selectedLineIdx = num;
+    console.log('working on line ', gMeme.selectedLineIdx)
 }
